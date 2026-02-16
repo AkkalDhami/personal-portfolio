a082f5841bdce3166f640f3b1906f3c5b6c8c446
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-16 11:15:48 +0545

    refactor: remove unused SearchIcon import from command.tsx and add new template image

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Dialog as CommandDialogPrimitive } from "@base-ui/react/dialog";
-import { SearchIcon } from "lucide-react";
 import type * as React from "react";
 import { cn } from "@/lib/utils";
 import {
